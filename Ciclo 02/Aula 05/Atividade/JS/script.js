89d49b3fc06053840841c480204008b50a9e6ab0
mayconwisley <maycon.wisley@outlook.com>
2021-08-26 09:31:34 -0300

    ajustes

--- Ciclo 02/Aula 05/Atividade/JS/script.js
+++ Ciclo 02/Aula 05/Atividade/JS/script.js
@@ -1,4 +1,8 @@
+import {msgLog} from '../Modulos/script.js';
 window.onload = function () {
+    //import { msgLog, Produtos } from "./Modulos/script.js";
+    msgLog("Teste");
+
     const btn = document.querySelector("#btn");
     const txt = document.querySelector("#txtTexto");;
     const link = document.querySelector("#link");
@@ -14,6 +18,26 @@ window.onload = function () {
     txt.addEventListener('click', () => {
         txt.value = "";
     });
+
+
+    var n = 'ee';
+
+    try {
+        n = Number(n);
+        if (isNaN(n)) {
+            throw "Este valor não é um numero";
+        }
+
+    } catch (error) {
+        console.log(`Erro: ${error}`);
+    }
+    finally {
+        console.log("Terminando");
+    }
+
+
+
+
 
 
 };
