cad99281ba048fc454dabecd9d2d560af2eafe56
mayconwisley <maycon.wisley@outlook.com>
2021-09-01 17:58:16 -0300

    Ciclo 04 Aula 1

---
+++ Ciclo 03-04/Projeto/Atividade/front-end/src/pages/Home/index.js
@@ -0,0 +1,8 @@
+export const Home = () =>{
+    return(
+        <div>
+            <h1>Pagina Inicial</h1>
+
+        </div>
+    )
+}
---
+++ Ciclo 03-04/Projeto/Atividade/front-end/src/pages/VisualizarCliente/index.js
@@ -0,0 +1,8 @@
+export const VisualizarCliente = () =>{
+    return(
+        <div>
+            <h1>Visualizar Cliente</h1>
+
+        </div>
+    )
+}
